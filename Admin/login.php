7d3e12794866635e8bc7f44abcd6b2bbd33d678b
Ravi <raviyadav2017sln@gmail.com>
2021-05-25 20:48:34 +0530

    second modified

--- Admin/login.php
+++ Admin/login.php
@@ -53,8 +53,6 @@ if(!isset($_SESSION['is_adminlogin'])){
           <div class="form-group">
             <i class="fas fa-user"></i><label for="email" class="pl-2 font-weight-bold">Email</label><input type="email"
               class="form-control" placeholder="Email" name="admin_email">
-            <!--Add text-white below if want text color white-->
-            <small class="form-text">We'll never share your email with anyone else.</small>
           </div>
           <div class="form-group">
             <i class="fas fa-key"></i><label for="pass" class="pl-2 font-weight-bold">Password</label><input type="password"
